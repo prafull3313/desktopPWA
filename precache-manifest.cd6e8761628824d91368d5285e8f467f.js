4ff1941a73fbee5ab0d4aa3da6f3eb76b52bc16f
Prafull Bansode <Prafull.Bansode@Staples.com>
2019-07-11 21:17:55 +0530

    Updates

---
+++ precache-manifest.cd6e8761628824d91368d5285e8f467f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb9076204cfac44cfe89bbd6bc2c831",
+    "url": "/desktopPWA/index.html"
+  },
+  {
+    "revision": "ce7f53cea0aeb9709c08",
+    "url": "/desktopPWA/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/desktopPWA/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ce7f53cea0aeb9709c08",
+    "url": "/desktopPWA/static/js/main.2d467d03.chunk.js"
+  },
+  {
+    "revision": "7d664780eeb82440fb2f",
+    "url": "/desktopPWA/static/js/runtime~main.4b814943.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/desktopPWA/static/media/logo.5d5d9eef.svg"
+  }
+]);
